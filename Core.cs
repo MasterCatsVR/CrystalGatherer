d06f69d7e13580f7fb1ce9c8920ddef80b64ca84
MasterCatsVR <75042082+MasterCatsVR@users.noreply.github.com>
2026-08-09 18:13:05 -0700

    stoned

--- Core.cs
+++ Core.cs
@@ -20,22 +20,24 @@ namespace crystalGatherer
 
             if (earlyGame = true)
             {
-               var theSkill = 38322u
+               var theSkill = 38322u // Basic Stone Gatherer
+               var skillType = "Basic Stone"
             }
             else
             {
-               var theSkill = 43020u
+               var theSkill = 43020u // Ore Gatherer
+               var skillType = "Ore"
             }
 
             LoggerInstance.Msg("Late Initialized.");
-            ProfessionSkill skill = ProfessionSkill.All.Where(skill => skill.Hash == theSkill).First(); // The Skill
+            ProfessionSkill skill = ProfessionSkill.All.Where(skill => skill.Hash == theSkill).First(); // one of the skills
             Gatherer oreGatherer = skill as Gatherer;
             Item[] validItems = (Item[])oreGatherer.GetType().GetField("validItems", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(oreGatherer);
             List<Item> validItemList = validItems.ToList();
             validItemList.AddAll(Item.All.Where(item => item.Hash == 45754u || item.Hash == 7824u));
 
             oreGatherer.GetType().GetField("validItems", BindingFlags.Instance | BindingFlags.NonPublic).SetValue(oreGatherer, validItemList.ToArray());
-            LoggerInstance.Msg("Ore Gatherer skill updated with new valid items.");
+            LoggerInstance.Msg(skillType + " Gatherer skill updated with new valid items.");
         }
     }
 }
