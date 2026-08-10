31f75fb3bb600af8c0592edb269b6f7245fa958a
MasterCatsVR <75042082+MasterCatsVR@users.noreply.github.com>
2026-08-09 19:18:12 -0700

    Bump v1.1.0

--- Core.cs
+++ Core.cs
@@ -2,29 +2,47 @@
 using Alta.Inventory;
 using System.Reflection;
 
-[assembly: MelonInfo(typeof(crystalGatherer.Core), "CrystalGatherer", "1.0.0", "MasterCats", null)]
+[assembly: MelonInfo(typeof(crystalGatherer.Core), "CrystalGatherer", "1.1.0", "MasterCats", null)]
 [assembly: MelonGame("Alta", "A Township Tale")]
 
 namespace crystalGatherer
 {
     public class Core : MelonMod
     {
+
+        private MelonPreferences_Category category;
+        private MelonPreferences_Entry<bool> earlyGame;
+        uint theSkill;
+        string skillType;
         public override void OnInitializeMelon()
         {
+            category = MelonPreferences.CreateCategory("CrystalGatherer");
+            earlyGame = category.CreateEntry<bool>("StoneGatherer", false); // defaults to false, making it ore gatherer.
+            MelonPreferences.Save(); 
+
+            if (earlyGame.Value)
+            {
+               theSkill = 38322u; // Basic Stone Gatherer
+               skillType = "Basic Stone";
+            }
+            else
+            {
+               theSkill = 43020u; // Ore Gatherer
+               skillType = "Ore";
+            }
             LoggerInstance.Msg("Initialized.");
         }
         public override void OnLateInitializeMelon()
         {
             LoggerInstance.Msg("Late Initialized.");
-            ProfessionSkill skill = ProfessionSkill.All.Where(skill => skill.Hash == 43020u).First(); // this is Ore Gatherer
+            ProfessionSkill skill = ProfessionSkill.All.Where(skill => skill.Hash == theSkill).First(); // edits either of the skills above depending on the config.
             Gatherer oreGatherer = skill as Gatherer;
             Item[] validItems = (Item[])oreGatherer.GetType().GetField("validItems", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(oreGatherer);
             List<Item> validItemList = validItems.ToList();
             validItemList.AddAll(Item.All.Where(item => item.Hash == 45754u || item.Hash == 7824u));
 
             oreGatherer.GetType().GetField("validItems", BindingFlags.Instance | BindingFlags.NonPublic).SetValue(oreGatherer, validItemList.ToArray());
-            LoggerInstance.Msg("Ore Gatherer skill updated with new valid items.");
+            LoggerInstance.Msg(skillType + " Gatherer skill updated with new valid items.");
         }
     }
 }
-## Bump v1.0.0
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-Adds crystals and shards to the ore gatherer skill. That's it. 
+# Crystal Gatherer
+
+Adds crystals and crystal shards to the various mining gathering skills.
+
+## Toggles!
+
+Inside `MelonPreferences.cfg` you can toggle it from the Ore Gatherer skill or the Stone Gatherer skill.
+
+By default it's on false, meaning it uses the Ore Gatherer skill. Set it to true and it uses the Stone Gatherer skill.
