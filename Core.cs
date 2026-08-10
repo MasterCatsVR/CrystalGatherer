6334c144cdcc715f18bab097e6357ba98742f9ea
MasterCatsVR <75042082+MasterCatsVR@users.noreply.github.com>
2026-08-09 19:10:53 -0700

    done.

--- Core.cs
+++ Core.cs
@@ -2,35 +2,40 @@
 using Alta.Inventory;
 using System.Reflection;
 
-[assembly: MelonInfo(typeof(crystalGatherer.Core), "CrystalGatherer", "1.0.0", "MasterCats", null)]
+[assembly: MelonInfo(typeof(crystalGatherer.Core), "CrystalGatherer", "1.1.0", "MasterCats", null)]
 [assembly: MelonGame("Alta", "A Township Tale")]
 
 namespace crystalGatherer
 {
     public class Core : MelonMod
     {
+
+        private MelonPreferences_Category category;
+        private MelonPreferences_Entry<bool> earlyGame;
+        uint theSkill;
+        string skillType;
         public override void OnInitializeMelon()
         {
             category = MelonPreferences.CreateCategory("CrystalGatherer");
-            earlyGame = ourFirstCategory.CreateEntry<bool>("Early Game", false);
+            earlyGame = category.CreateEntry<bool>("StoneGatherer", false); // defaults to false, making it ore gatherer.
+            MelonPreferences.Save(); 
+
+            if (earlyGame.Value)
+            {
+               theSkill = 38322u; // Basic Stone Gatherer
+               skillType = "Basic Stone";
+            }
+            else
+            {
+               theSkill = 43020u; // Ore Gatherer
+               skillType = "Ore";
+            }
             LoggerInstance.Msg("Initialized.");
         }
         public override void OnLateInitializeMelon()
         {
-
-            if (earlyGame = true)
-            {
-               var theSkill = 38322u; // Basic Stone Gatherer
-               var skillType = "Basic Stone";
-            }
-            else
-            {
-               var theSkill = 43020u; // Ore Gatherer
-               var skillType = "Ore";
-            }
-
             LoggerInstance.Msg("Late Initialized.");
-            ProfessionSkill skill = ProfessionSkill.All.Where(skill => skill.Hash == theSkill).First(); // one of the skills
+            ProfessionSkill skill = ProfessionSkill.All.Where(skill => skill.Hash == theSkill).First(); // edits either of the skills above depending on the config.
             Gatherer oreGatherer = skill as Gatherer;
             Item[] validItems = (Item[])oreGatherer.GetType().GetField("validItems", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(oreGatherer);
             List<Item> validItemList = validItems.ToList();
